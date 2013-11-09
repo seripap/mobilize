bf38b5700fd3d4cd748857c121627da8857b4342
Daniel Seripap <daniel.seripap@weather.com>
2013-11-09 09:59:43 -0500

    Added grid system, changd route

---
+++ backend/application/views/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Youth Help - LNF</title>
+  <link rel="stylesheet" type="text/css" href="css/screen.css">
+  <link rel="stylesheet" type="text/css" href="css/grid.css">
+  <link rel="stylesheet" type="text/css" href="css/branding.css">
+</head>
+<body>
+
+<div class="wrapper main-content">
+<section class="columns-12">
+TEST
+</section>
+
+</div>
+
+</body>
+</html>
