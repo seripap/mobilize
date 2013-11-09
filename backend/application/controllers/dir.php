0112f04cdfcb8c302f4fb8eb7c4e6b6a9dcf375c
Bret Newcome <newcome.brett@gmail.com>
2013-11-09 13:50:38 -0500

    Directory: Added Haskids and started view

--- backend/application/controllers/dir.php
+++ backend/application/controllers/dir.php
@@ -4,7 +4,31 @@ class dir extends CI_Controller {
 
 	public function index()
 	{
-		$cats_query = $this->db->query('SELECT * FROM directory_cat where active=1 order by ord,cid asc;');
+		$data["echo"] = "Jennifer's awesome";
+		$data['cats'] = $this->getcats();
+
+		foreach ($data['cats'] as $key => $cat) {
+			$data['cats'][$key]->haskids = $this->haskids($cat->cid);
+		}
+		$data['title'] = "Services";
+		$this->_render_page("dir", $data);
+		//$this->load->view('view/directory', $data, FALSE);
+	}
+	function haskids($cid){
+		$count_query = $this->db->query('SELECT * FROM directory_cat where active=1 and parent = '.$cid.'');
+		$count = count($count_query->result());
+		if($count>0){
+
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public function getcats($parent = 0, $format="data")
+	{
+		
+		$cats_query = $this->db->query('SELECT * FROM directory_cat where active=1 and parent='.$parent.' order by ord,cid asc;');
 		$cats = $cats_query->result();
 		$entries = array();
 		foreach ($cats as $key => $cat) {
@@ -16,23 +40,47 @@ class dir extends CI_Controller {
 			
 		}
 		$data['json'] = $cats;
-		$this->load->view('dir', $data, FALSE);
+		if($format ==="json"){
+			$this->load->view('getcats', $data, FALSE);
+		}elseif($format === "data"){
 
-
-		//$this->load->view('view/directory', $data, FALSE);
-	}
-	public function getentries ($cat){
-
-		echo "dan's awesome<br /><br />";
-
-		$entries_query = $this->db->query('SELECT * FROM directory_entries where active=1 and cid = '.$cat.' order by ord,id');
-		$entries[$cat] = $entries_query->result();
-		foreach ($entries[$cat] as $k => $entry) {
-			//echo $entry->title."<br />";
+			return $cats;
 		}
+
+	}
+	public function getentries ($cat, $format = "data"){
+
+		//echo "dan's awesome<br /><br />".$cat;
+		$entries_query = $this->db->query('SELECT * FROM directory_entries where active=1 and cid='.$cat.' order by ord,id');
+		$entries = $entries_query->result();
 		$data['json'] = $entries;
+		$this->load->view('getentries', $data, FALSE);
+	}
+	public function getentry($entry, $format = "data")
+	{
+		$entry_query = $this->db->query('SELECT * FROM directory_entries where active=1 and id='.$entry.'');
+		$entry = $entry_query->result();
+		$data['json'] = $entry;
+		$this->load->view('getentry', $data, FALSE);
+	}
+	public function rateentry($entry,$rate,$format = "data")
+	{
+		$user = $this->ion_auth->user()->row();
+		if($user){
+			$rate_query = $this->db->query("INSERT INTO `ratings` (`rate`,`entry`,`date`,`date_mod` ,`type`, `active`,`user`) VALUES ('".$rate."', '".$entry."',Now(),Now(),'entry','1', '".$user->id."')");
+		}
+	}
+	function _render_page($view, $data=null, $render=false)
+	{
+
+		$this->viewdata = (empty($data)) ? $this->data: $data;
+
+		$this->load->view('header');
+		$view_html = $this->load->view($view, $this->viewdata, $render);
+		$this->load->view('footer');
+
+		if (!$render) return $view_html;
 	}
 }
-
 /* End of file directory.php */
 /* Location: ./application/controllers/directory.php */
--- backend/application/views/dir.php
+++ backend/application/views/dir.php
@@ -1,3 +1,14 @@
-<?php
-$this->output->set_content_type('application/json')->set_output(json_encode($json));
+<h1><? echo $title;?></h1>
+
+<ul> 
+<?
+foreach ($cats as $key => $cat) {
+	echo "<li>".
+	
+	 "<span class='actiion' id='".$cat->cid."' rel='".($cat->haskids ?  "getkids" :  "getentries")."'>".$cat->title . "".
+	
+	 "</li>";
+}
+
 ?>
+</ul>
