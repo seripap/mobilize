3b6574609f6fdd6ff216e8d43b2c1efa6bf0bc0a
Daniel Seripap <daniel.seripap@weather.com>
2013-11-09 18:36:09 -0500

    Almost working chat

--- backend/application/views/chat.php
+++ backend/application/views/chat.php
@@ -1,7 +1,79 @@
 <script>
     var userid = <?php echo $user->id ?>;
+
+		var time = 0;
+    var helperid = $.segment(3);
+    var memData = '';;
+
+    if (!helperid) {
+      helperid = 1;
+    }
+
+    var updateTime = function (cb) {
+      $.getJSON("http://local.o0h.org/index.php/chat/time", function (data) {
+        cb(~~data);
+      });
+    };
+
+    var sendChat = function (message) {
+      $.getJSON("http://local.o0h.org/index.php/chat/insert_chat?message=" + message + "&from=" + userid + "&to=" + helperid + "&convoid=" + helperid, function (){
+        $('#message_container').html('');
+        getNewChats();
+      });
+    }
+
+    var addDataToReceived = function (arrayOfData) {
+      arrayOfData.forEach(function (data) {
+        $("#received").val($("#received").val() + "\n" + data[0]);
+      });
+    }
+
+    var getNewChats = function () {
+      $.getJSON("http://local.o0h.org/index.php/chat/get_chats?convoid=" + helperid, function (data){
+        console.debug(data);
+        var html = '';
+
+          $.each(data, function() {
+            if (this.to_user_id != userid) {
+              html += ('<em id="1">'+this.my_username+'</em> - ');
+              html += ('<span id="1">'+this.message+'</span>');
+              html += ('<br>');
+            } else {
+              html += ('<span>'+this.message+'</span>');
+              html += (' - <strong>'+this.my_username+'</strong> ');
+              html += ('<br>');
+            }
+          });
+            $('#message_container').html(html);
+
+
+        // reset scroll height
+        // setTimeout(function(){
+        //    $('#received').scrollTop($('#received')[0].scrollHeight);
+        // }, 0);
+    });
+    }
+
+    // using JQUERY's ready method to know when all dom elements are rendered
+    $( document ).ready ( function () {
+      getNewChats();
+      // set an on click on the button
+      $("form").submit(function (evt) {
+        evt.preventDefault();
+        var data = $("#text").val();
+        $("#text").val('');
+
+        // get the time if clicked via a ajax get queury
+        sendChat(data);
+      });
+
+      setInterval(function (){
+        getNewChats();
+      },1500);
+    });
+
 </script>
-<script src="http://local.o0h.org/js/chat.js"></script>
+
   <section id="message_container">
   </section>
 
