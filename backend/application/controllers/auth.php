a20d7ef3d674214e4d3f7983f434fc0eb0920542
Daniel Seripap <daniel.seripap@weather.com>
2013-11-09 12:53:32 -0500

    Index auth

--- backend/application/controllers/auth.php
+++ backend/application/controllers/auth.php
@@ -26,21 +26,24 @@ class Auth extends CI_Controller {
 			//redirect them to the login page
 			redirect('login', 'refresh');
 		}
-		elseif (!$this->ion_auth->is_admin()) //remove this elseif if you want to enable this for non-admins
-		{
-			//redirect them to the home page because they must be an administrator to view this
-			return show_error('You must be an administrator to view this page.');
-		}
 		else
 		{
 			//set the flash data error message if there is one
 			$this->data['message'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
 
-			//list the users
-			$this->data['users'] = $this->ion_auth->users()->result();
-			foreach ($this->data['users'] as $k => $user)
-			{
-				$this->data['users'][$k]->groups = $this->ion_auth->get_users_groups($user->id)->result();
+			// # single group (by name)
+			// $group = 'admin';
+			// if (!$this->ion_auth->in_group($group))
+			// {
+			// 	$this->data['message'] = $this->session->set_flashdata('message', 'You must be a admin to view this page');
+			// }
+
+			$this->data['user'] = $this->ion_auth->user()->row();
+
+			if (!$this->ion_auth->is_admin()) {
+				$this->data['admin'] = FALSE;
+			} else {
+				$this->data['admin'] = TRUE;
 			}
 
 			$this->_render_page('index', $this->data);
