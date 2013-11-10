1ae9b053c99ffd534f3d8c5dcc211c7b87a8c559
Matt Black <matt.black@weather.com>
2013-11-10 14:46:01 -0500

    Attempt to fix input focus

--- backend/application/views/footer.php
+++ backend/application/views/footer.php
@@ -14,10 +14,17 @@
 	<script type="text/javascript" src="/js/iscroll.js"></script>
 	<script>
 		// Fix for Search page scrolling up and getting stuck on iPad -->
+		/*
 		$('input').focus(rePos(this));
 		function rePos(element){
-			window.scrollTo(0, 1);
+			window.scrollTo(0, '50px');
 		}
+		*/
+
+		$('input').focus(function(){
+			window.scrollTo(0, 300)
+			console.log('scrollto')
+		})
 	</script>
 </body>
 </html>
