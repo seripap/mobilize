8ca1b2afd67bbd6fa85d212496e1a6695f72670e
Matt Black <matt.black@weather.com>
2013-11-10 13:49:27 -0500

    Settings page?

--- backend/application/views/settings.php
+++ backend/application/views/settings.php
@@ -3,3 +3,16 @@
 <p style="text-align: center;"><a href="http://o0h.org/index.php/logout">Logout</a></p>
     </p></div>
 </div>
+
+<center>
+	<div class="login-button lnfaccount" style="top: 25px;">
+	    <div class="table-wrapper">
+	        <div class="icon-block">
+	            <span class="icon icon-user"></span>
+	        </div>
+	        <div class="title-block"> 
+	            <a href="http://o0h.org/index.php/logout" style="color:white; text-decoration:none">Logout</a>
+	        </div>
+	    </div>
+	</div>
+</center>
