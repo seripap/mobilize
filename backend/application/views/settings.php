f9e92906a289bdb1157d391b351e7b3e87e9f9ee
Matt Black <matt.black@weather.com>
2013-11-10 13:50:30 -0500

    Meh

--- backend/application/views/settings.php
+++ backend/application/views/settings.php
@@ -1,11 +1,6 @@
-<div class="context-wrapper" style="width: 320px;">
-    <div class="context-block" style="padding-top: 15px;">
-<p style="text-align: center;"><a href="http://o0h.org/index.php/logout">Logout</a></p>
-    </p></div>
-</div>
-
 <center>
-	<div class="login-button lnfaccount" style="top: 25px;">
+	<br/><br/>
+	<div class="login-button lnfaccount">
 	    <div class="table-wrapper">
 	        <div class="icon-block">
 	            <span class="icon icon-user"></span>
