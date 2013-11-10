532640b581a1233b53c4a29b4227f7ea0438a808
Matt Black <matt.black@weather.com>
2013-11-10 13:51:20 -0500

    Another meh

--- backend/application/views/settings.php
+++ backend/application/views/settings.php
@@ -2,11 +2,8 @@
 	<br/><br/>
 	<div class="login-button lnfaccount">
 	    <div class="table-wrapper">
-	        <div class="icon-block">
-	            <span class="icon icon-user"></span>
-	        </div>
 	        <div class="title-block"> 
-	            <a href="http://o0h.org/index.php/logout" style="color:white; text-decoration:none">Logout</a>
+	            <a href="http://o0h.org/index.php/logout" style="color:white; text-decoration:none; text-align: center;">Logout</a>
 	        </div>
 	    </div>
 	</div>
