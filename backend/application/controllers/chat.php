b09f3248c4aa5d0ba75a4b1340d1e7e3ac8224fd
Bret Newcome <newcome.brett@gmail.com>
2013-11-10 09:09:51 -0500

    working pushes

--- backend/application/controllers/chat.php
+++ backend/application/controllers/chat.php
@@ -34,11 +34,53 @@ class Chat extends CI_Controller {
 	public function insert_chat() {
 
 		/* Create a table if it doesn't exist already */
-		$this->Chat_model->create_table();
+		//$this->Chat_model->create_table();
 
-		$this->Chat_model->insert_message($_REQUEST["message"], $_REQUEST["from"], $_REQUEST["to"], $_REQUEST["convoid"]);
+		//$this->Chat_model->insert_message($_REQUEST["message"], $_REQUEST["from"], $_REQUEST["to"], $_REQUEST["convoid"]);
+ define('APPKEY','ES-nFtk6QBuHX7EEEPfW9A'); // Your App Key
+ define('PUSHSECRET', '4QOndPlnRW688Np-laAxEg'); // Your Master Secret
+ define('PUSHURL', 'https://go.urbanairship.com/api/push/');
+		$contents = array();
+ $contents['badge'] = "+1";
+ $contents['alert'] = "New Message: ".$_REQUEST["message"];
+ $extras = array();
 
-		echo json_encode('done');
+ $extras['url']="https://Blah";
+ $contents['extra'] = $extras;
+ $notification = array();
+ $notification['ios'] = $contents;
+ $platform = array();
+ array_push($platform, "ios");
+
+ $push = array("audience"=>"all", "notification"=>$notification, "device_types"=>$platform);
+
+
+
+ $json = json_encode($push);
+echo $json ."<br/ >";
+ $session = curl_init(PUSHURL);
+ curl_setopt($session, CURLOPT_USERPWD, APPKEY . ':' . PUSHSECRET);
+ curl_setopt($session, CURLOPT_POST, True);
+ curl_setopt($session, CURLOPT_POSTFIELDS, $json);
+ curl_setopt($session, CURLOPT_HEADER, False);
+ curl_setopt($session, CURLOPT_RETURNTRANSFER, True);
+ curl_setopt($session, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Accept: application/vnd.urbanairship+json; version=3;'));
+ $content = curl_exec($session);
+ echo $content; // just for testing what was sent
+
+ // Check if any error occured
+ $response = curl_getinfo($session);
+ if($response['http_code'] != 202) {
+     echo "Got negative response from server, http code: ".
+     $response['http_code'] . "\n";
+ } else {
+
+     echo "Wow, it worked!\n";
+ }
+
+ curl_close($session);
+
+		
 	}
 
 	public function time() {
