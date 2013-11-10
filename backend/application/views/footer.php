6b9dd6f98264569b020c51c692ac2df3401f4022
Matt Black <matt.black@weather.com>
2013-11-09 19:41:28 -0500

    Add HTML to header and footer; add iscroll

--- backend/application/views/footer.php
+++ backend/application/views/footer.php
@@ -1,3 +1,20 @@
-</div>
+			</section> 
+		</div> 
+ 
+		<!-- Project Footer --> 
+		<footer>
+			<ul>
+				<li>Chat</li>
+				<li>Services</li>
+				<li>About</li>
+			</ul>
+		</footer>
+	</div>
+
+	<script type="text/javascript" src="/js/iscroll.js"></script>
+	<script>
+		bodyScroll = new iScroll("wrapper-content", { bounce:true, momentum:true, preventGhostClick:true});
+		document.addEventListener("touchmove", function (e) { e.preventDefault(); }, false);
+	</script>
 </body>
 </html>
